f4a24a95cae617f8abf79ce60621a3b58a75c3dd
mfnms-iscteiul <miguelmartins737@gmail.com>
2021-01-18 08:24:29 +0000

    Search Screen Beggining

---
+++ src/screens/loggedIn/search/_index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import auth from '@react-native-firebase/auth';
+import LoadingScreen from '../../loading';
+import {createStackNavigator} from '@react-navigation/stack';
+import SearchScreen from './searchScreen';
+import SearchGroupScreen from './searchGroupScreen';
+
+
+function SearchContentScreen() {
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setUser(auth().currentUser);
+    setLoading(false);
+  }, []);
+
+  const Stack = createStackNavigator();
+  return (
+    <>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <View style={{height: '100%'}}>
+          <Stack.Navigator initialRouteName="search">
+            <Stack.Screen
+              options={{headerShown: false, headerBackTitleVisible: false}}
+              name="search"
+              component={SearchScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false, headerBackTitleVisible: false}}
+              name="searchGroup"
+              component={SearchGroupScreen}
+            />
+          </Stack.Navigator>
+        </View>
+      )}
+    </>
+  );
+}
+export default SearchContentScreen;
